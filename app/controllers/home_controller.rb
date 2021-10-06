b622df696630a69c2068ae7547d3e460c3310190
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-10-06 06:17:09 +0200

    customize bulma

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @subjects = Subject.all
+    if slug = params[:subject]
+      @subject = Subject.find_by(slug: slug)
+      @courses = Course.where(subject: @subject)
+    else
+      @courses = Course.all
+    end
   end
 end
