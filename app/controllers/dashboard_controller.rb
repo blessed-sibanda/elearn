58f62bc93d477dc8254f053979bc42695d8189c7
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-10-06 17:31:37 +0200

    active nav link helpers

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,21 +1,16 @@
 class DashboardController < ApplicationController
   def index
-    @active = "index"
   end
 
   def courses
-    @active = "courses"
   end
 
   def students
-    @active = "students"
   end
 
   def notifications
-    @active = "notifications"
   end
 
   def messages
-    @active = "messages"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  def is_home_page?
+    controller.controller_name == "home"
+  end
+
+  def is_dashboard_page?
+    controller.controller_name == "dashboard"
+  end
+
+  def is_dashboard_index_page?
+    is_dashboard_page? && controller.action_name == "index"
+  end
+
+  def is_dashboard_students_page?
+    is_dashboard_page? && controller.action_name == "students"
+  end
+
+  def is_dashboard_courses_page?
+    is_dashboard_page? && controller.action_name == "courses"
+  end
+
+  def is_dashboard_notifications_page?
+    is_dashboard_page? && controller.action_name == "notifications"
+  end
+
+  def is_dashboard_messages_page?
+    is_dashboard_page? && controller.action_name == "messages"
+  end
 end
