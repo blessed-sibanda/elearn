feb81e125e627de9b951de97a61fb5ca6f743a24
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-10-08 08:42:34 +0200

    enrollment model spec

--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -18,8 +18,11 @@
 #  fk_rails_...  (course_id => courses.id)
 #  fk_rails_...  (user_id => users.id)
 #
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Enrollment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to(:user) }
+    it { should belong_to(:course) }
+  end
 end
